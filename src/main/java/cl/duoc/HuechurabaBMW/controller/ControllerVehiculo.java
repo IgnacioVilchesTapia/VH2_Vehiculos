109dee41b5975f446aff95430ca37da23bf73b96
Miguel González <migu.gonzalezc@duocuc.cl>
2026-04-27 11:54:52 -0400

    cambios

---
+++ src/main/java/cl/duoc/HuechurabaBMW/controller/ControllerVehiculo.java
@@ -0,0 +1,5 @@
+package cl.duoc.HuechurabaBMW.controller;
+
+public class ControllerVehiculo {
+
+}
---
+++ src/main/java/cl/duoc/HuechurabaBMW/repository/RepositoryVehiculo.java
@@ -0,0 +1,5 @@
+package cl.duoc.HuechurabaBMW.repository;
+
+public class RepositoryVehiculo {
+
+}
