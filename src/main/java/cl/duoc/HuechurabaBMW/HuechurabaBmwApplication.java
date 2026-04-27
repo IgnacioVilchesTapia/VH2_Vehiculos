b5cd25a3f19b369fd0724f1f4033bc1dac30b7b2
unknown <Cetecom@SJAPCA03LC0631p.duoc.acad>
2026-04-27 11:25:54 -0400

    first commit

---
+++ src/main/java/cl/duoc/HuechurabaBMW/HuechurabaBmwApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.HuechurabaBMW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuechurabaBmwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuechurabaBmwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/HuechurabaBMW/HuechurabaBmwApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.HuechurabaBMW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuechurabaBmwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
