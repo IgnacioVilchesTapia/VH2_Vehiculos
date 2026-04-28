6dae2e5efa891cc1f57d930fd0a1a0661192329e
Miguel González <migu.gonzalezc@duocuc.cl>
2026-04-28 10:51:24 -0400

    cambios

---
+++ src/main/java/cl/duoc/HuechurabaBMW/dto/ApiResponse.java
@@ -0,0 +1,12 @@
+package cl.duoc.HuechurabaBMW.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
+public class ApiResponse <T> {
+    private String message;
+    private T data;
+}
