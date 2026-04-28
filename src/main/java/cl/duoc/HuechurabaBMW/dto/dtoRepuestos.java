6d1b7f1023b50c0d95f7e21100273abd4737c867
unknown <Cetecom@SJAPCA03LC0629p.duoc.acad>
2026-04-28 11:01:23 -0400

    Agrego DTO

--- src/main/java/cl/duoc/HuechurabaBMW/dto/dtoRepuestos.java
+++ src/main/java/cl/duoc/HuechurabaBMW/dto/dtoRepuestos.java
@@ -1,5 +1,16 @@
 package cl.duoc.HuechurabaBMW.dto;
 
-public class dtoRepuestos {
+import lombok.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgConstructor
+
+public class dtoRepuestos {
+    private Long id;
+    private String marca;
+    private String nombre;
+    private int stock;
+    private double precio;
 }
