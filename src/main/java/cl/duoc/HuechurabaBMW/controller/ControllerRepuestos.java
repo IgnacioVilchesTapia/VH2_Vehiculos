b452e70779d01c4fb1adcff4ade957ca5e119396
Miguel González <migu.gonzalezc@duocuc.cl>
2026-04-29 23:41:55 -0400

    algunos cambios en controller

--- src/main/java/cl/duoc/HuechurabaBMW/controller/ControllerRepuestos.java
+++ src/main/java/cl/duoc/HuechurabaBMW/controller/ControllerRepuestos.java
@@ -16,31 +16,26 @@ public class ControllerRepuestos {
     @Autowired
     private ServiceRepuestos service;
 
-    // Crear
     @PostMapping
     public ModelRepuestos agregarRepuesto(@RequestBody DTORepuestos dto) {
         return service.save(dto);
     }
 
-    // Listar
     @GetMapping
     public List<ModelRepuestos> obtenerRepuestos() {
         return service.listar();
     }
 
-    // Ver stock
     @GetMapping("/{id}/stock")
     public String verStock(@PathVariable Long id) {
         return "Stock disponible: " + service.buscarPorId(id).getStock();
     }
 
-    // Vender
     @PutMapping("/{id}/vender")
     public ModelRepuestos vender(@PathVariable Long id) {
         return service.vender(id);
     }
 
-    // Agregar stock
     @PutMapping("/{id}/agregar")
     public ModelRepuestos agregarStock(@PathVariable Long id,
                                        @RequestParam int cantidad) {
